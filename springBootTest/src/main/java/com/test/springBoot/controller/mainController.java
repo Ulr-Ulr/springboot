a1407e8c97dbc030e5babdab7f9c22470c3c6180
G <G@LAPTOP-EG6G9I10>
2021-08-09 10:16:58 +0900

    210809

--- springBootTest/src/main/java/com/test/springBoot/controller/mainController.java
+++ springBootTest/src/main/java/com/test/springBoot/controller/mainController.java
@@ -4,13 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.test.springBoot.vo.testVO;
+
 @Controller
 public class mainController {
 
-@RequestMapping("/main/home")	
-@ResponseBody
-public String MainController() {
-	
-	return "HelloWorld";
-}
+	@RequestMapping("/")
+	public String mainPage() {
+		return "index";
+	}
+	@RequestMapping("/vo")
+	@ResponseBody
+	public testVO test(testVO id) {
+		
+		return id;
+	}
 }
